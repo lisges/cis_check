acffdcdd19d42687dda5d174f54b8b94b65db1aa
peterj <peter@lisg.es>
2014-05-09 15:13:18 +0200

    Grades are inserted into hash now. Removed manual regex, going to use a temp file for changes.

--- cis_check.pl
+++ cis_check.pl
@@ -7,9 +7,10 @@ binmode(STDOUT, ":utf8");
 #### Enter your credentials here ####
 my $username = "XXXXX";
 my $password = "Password";
-my $regex = "Englisch";
 #####################################
-my (%examsCredit, %examsGrade) = (); # Key: exam name, Value: cp - Key: exam name, Value: grade 
+my (%examsCredit, %examsGrade) = ();
+# %examsCredit: Key: exam name, Value: cp
+# %examsGrade: Key: exam name, Value: grade 
 my (@matches, @matches2);
 my $out;
 print "Initialize WebAgent...\n";
@@ -22,24 +23,9 @@ $mech -> field ('user' => $username);
 $mech -> field ('pass' => $password);
 $mech -> click ('submit');
 print "Fetching results...\n";
-# Get all evaluated exams and grades:
+# Get data and regex pairs into hash map:
 $mech->get( "https://cis.nordakademie.de/pruefungsamt/pruefungsergebnisse/" );
-$out = $mech->content;
-@matches = $out =~ /value=\'([1-5].[0-7])\'/g;
-# Get exams and cp:
+%examsGrade = $mech->content =~ /450><p class='noten_noten'>(.*?)<.*?value='([1-5]\.[0-7])/sg;
 $mech->get( "https://cis.nordakademie.de/pruefungsamt/studienplan/" );
-$out = $mech->content;
-# Regex exams and cp into array:
-@matches2 = $out =~ /g'>&nbsp;&nbsp;(.*?)<\/td>.*?cp.*?<span>([1-9])</g;
-# From array to hashmap
-for ($i=0;$i<@matches2;$i+=2) {
-	$examsCredit{ $matches2[$i] } = $matches2[$i+1];
-	print "Set key $matches2[$i] to $matches2[$i+1]\n";
-}
-
-if ($mech->content( format => 'text' ) =~ m/$regex/) {
-	print "Die Note ist da!\n";
-} else {
-	print "Die Note ist noch nicht da!\n";
-}
-printf("Dein Durchschnitt liegt bei: %.3f!\nBisher wurden %d Klausuren geschrieben.\n", sum(@matches) / @matches, $#matches+1);
+%examsCredit = $mech->content =~ /g'>&nbsp;&nbsp;(.*?)<\/td>.*?cp.*?<span>([1-9])</g;
+printf("Dein Durchschnitt liegt bei: %.3f!\nBisher wurden %d Klausuren geschrieben.\n", sum(values(%examsGrade)) / keys( %examsGrade ), keys( %examsGrade )+0);
