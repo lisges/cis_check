e6cfdbb3ed3971073491991ca3d72b0cc56d56e9
peterjl <peter@lisg.es>
2014-05-09 17:45:25 +0200

    Provides detailed info on grade, establish relation between exams, grades and credits.

--- cis_check.pl
+++ cis_check.pl
@@ -11,8 +11,7 @@ my $password = "Password";
 my (%examsCredit, %examsGrade) = ();
 # %examsCredit: Key: exam name, Value: cp
 # %examsGrade: Key: exam name, Value: grade 
-my (@matches, @matches2);
-my $out;
+my @sanitizeMe;
 print "Initialize WebAgent...\n";
 my $mech = WWW::Mechanize->new();
 $mech -> cookie_jar(HTTP::Cookies->new());
@@ -25,7 +24,24 @@ $mech -> click ('submit');
 print "Fetching results...\n";
 # Get data and regex pairs into hash map:
 $mech->get( "https://cis.nordakademie.de/pruefungsamt/pruefungsergebnisse/" );
-%examsGrade = $mech->content =~ /450><p class='noten_noten'>(.*?)<.*?value='([1-5]\.[0-7])/sg;
+@sanitizeMe = $mech->content =~ /450><p class='noten_noten'>(.*?)<.*?value='([1-5]\.[0-7])/sg;
+# Unfortunately the exam names differ, e.g. "Programmierung1" vs. "Programmierung 1"
+for (my $i=0; $i <= $#sanitizeMe; $i++)
+{
+	$sanitizeMe[$i] =~ s/(\S)([1-2])/$1 $2/;
+}
+%examsGrade = @sanitizeMe;
+
 $mech->get( "https://cis.nordakademie.de/pruefungsamt/studienplan/" );
 %examsCredit = $mech->content =~ /g'>&nbsp;&nbsp;(.*?)<\/td>.*?cp.*?<span>([1-9])</g;
-printf("Dein Durchschnitt liegt bei: %.3f!\nBisher wurden %d Klausuren geschrieben.\n", sum(values(%examsGrade)) / keys( %examsGrade ), keys( %examsGrade )+0);
+printf("Dein simpler Durchschnitt liegt bei: %.3f!\nBisher wurden %d Klausuren geschrieben.\n\nDeine Noten im Detail:\n\n", sum(values(%examsGrade)) / keys( %examsGrade ), keys( %examsGrade )+0);
+printf "%-56s %-12s %s\n", "Fach:", "Note:", "Credits:";
+foreach my $gradeKey ( keys %examsGrade )
+{
+	my $credit = 0;
+	if (exists $examsCredit{$gradeKey})
+	{	
+		$credit = $examsCredit{$gradeKey};
+	}
+	printf "%-56s %-12.1f %d\n", $gradeKey, $examsGrade{$gradeKey},$credit;
+}
